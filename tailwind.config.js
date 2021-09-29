5e3ba1b943ba9c0397edc8087aa144a5e5d0c3f9
alex-voltagency <80480705+alex-voltagency@users.noreply.github.com>
2021-09-29 12:43:43 -0400

    add structure

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@ module.exports = {
     purge: [],
     darkMode: false, // or 'media' or 'class'
     theme: {
-        extend: {},
+        extend: { 
+             colors: {
+               "color1" : '#733FC8',
+                "color2" : "#CFCFCF",
+                "color3" : "#48556A",
+                "color4" : "#19202D",
+             }
+        },
     },
     variants: {
         extend: {},
