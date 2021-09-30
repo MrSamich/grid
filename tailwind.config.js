de757790bf1930f4e1dfd8ca507769ced8153d49
alex-voltagency <80480705+alex-voltagency@users.noreply.github.com>
2021-09-30 00:18:14 -0400

    final commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,15 +3,33 @@ module.exports = {
     darkMode: false, // or 'media' or 'class'
     theme: {
         extend: { 
+            fontFamily: {
+            'barlow': ['Barlow Semi Condensed'],
+            },
+            fontSize: {
+                'fs3': '0.813rem',
+                'fs11': '0.688rem',
+            },
+            lineHeight: {
+                'lh18': '1.125rem',
+                'lh13': '0.813rem',
+                'lh2': '0.688rem',
+            },
+
              colors: {
                "color1" : '#733FC8',
                 "color2" : "#CFCFCF",
                 "color3" : "#48556A",
                 "color4" : "#19202D",
                 "color5" : "#A775F1",
+                "color6" : "#ECF2F8"
              },
              padding: {
+                'p18': '1.125rem',
                 'p6': '1.625rem',
+                'p4': '1.063rem',
+                'p17': '4.438rem',
+                
              },
         },
     },
